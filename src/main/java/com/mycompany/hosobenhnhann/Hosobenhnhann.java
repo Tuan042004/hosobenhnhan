3da51c8359016e1e094e37ee8090994b9771f605
Tuan042004 <tuanmanh793@gmail.com>
2024-09-30 14:36:30 +0700

    first commit

---
+++ src/main/java/com/mycompany/hosobenhnhann/Hosobenhnhann.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hosobenhnhann;
+
+/**
+ *
+ * @author Admin
+ */
+public class Hosobenhnhann {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
