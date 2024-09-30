f75c4449243d4ec39a636ebed948f40e1dbaf660
Tuan042004 <tuanmanh793@gmail.com>
2024-09-30 15:36:53 +0700

    ok roi

--- src/main/java/BTL/config.java
+++ src/main/java/BTL/config.java
@@ -3,11 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package BTL;
-
+import io.github.cdimascio.dotenv.Dotenv;
 /**
  *
  * @author Admin
  */
 public class config {
-    
+   public static Dotenv dotenv = Dotenv.load();
+   public static String db_url = dotenv.get("URL_DB");
+   public static String db_username = dotenv.get("USERNAME_DB");
+   public static String db_pass = dotenv.get("PASS_DB");
 }
